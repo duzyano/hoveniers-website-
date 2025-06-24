c0a84ec63f23682ec639634df002742c60b2507b
duzyano <Duzyanodrj@hotmail.com>
2025-06-24 15:09:40 +0100

    alles geregeld ga nu werken aan slideshow

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/slideshow.css">
   </head>
-  <body></body>
+  <body>
+    <?php include 'includes/slideshow.php'; ?>
+  </body>
 </html>
