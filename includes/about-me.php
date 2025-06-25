f20679091019cd8b2e94d48c50a7a49a91f99943
valeriooooooo <valeriomalagoli08@gmail.com>
2025-06-25 10:45:45 +0200

    We komen ergens

--- includes/about-me.php
+++ includes/about-me.php
@@ -1,5 +1,6 @@
+<section class="aboutme" id="aboutme">
 
-    <section class="aboutme">
-      <h3>About Me</h3>
-
-    </section>
+<p>De tuin is een belangrijke plek van de woning, waar je het liefst zoveel mogelijk tijd in doorbrengt. Bij Hendrik Hogendijk Hoveniers vindt ik het daarom belangrijk dat iedereen zich thuis voelt in zijn of haar tuin. Ik maak de tuin onderdeel van jouw "thuis", door hem volledig op jouw wensen af te stemmen.</p>
+<p>Met behulp van mooie, natuurlijke en duurzame producten en materialen creëer ik een tuin, die garant staat voor een jarenlang plezierig buitenleven. Van een knusse veranda en een mooie vijver, tot een gezellig terras en een kleurrijke bloemenborder: ik stop al mijn energie erin.</p>
+<img src="" alt="" class="image">
+</section>
