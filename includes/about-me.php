626d13d9db596880264014f64e8df549a2d2e834
valeriooooooo <valeriomalagoli08@gmail.com>
2025-06-25 10:53:41 +0200

    hendrik foto toegevoegd

--- includes/about-me.php
+++ includes/about-me.php
@@ -2,5 +2,5 @@
 
 <p>De tuin is een belangrijke plek van de woning, waar je het liefst zoveel mogelijk tijd in doorbrengt. Bij Hendrik Hogendijk Hoveniers vindt ik het daarom belangrijk dat iedereen zich thuis voelt in zijn of haar tuin. Ik maak de tuin onderdeel van jouw "thuis", door hem volledig op jouw wensen af te stemmen.</p>
 <p>Met behulp van mooie, natuurlijke en duurzame producten en materialen creëer ik een tuin, die garant staat voor een jarenlang plezierig buitenleven. Van een knusse veranda en een mooie vijver, tot een gezellig terras en een kleurrijke bloemenborder: ik stop al mijn energie erin.</p>
-<img src="" alt="" class="image">
+<img src="../assets/images/image.jpg" alt="" class="image">
 </section>
