6f87169d37e62a7261f27a170151adc0818c5e1f
duzyano <Duzyanodrj@hotmail.com>
2025-06-25 13:10:34 +0100

    footer bijna af

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="../assets/css/footer.css">
+<footer>
+
+<div class="div1">
+    <h3>Gegevens</h3>
+    <ul>
+        <li>06-21745678</li>
+        <li>Hoefstraat 42</li>
+        <li>7143HH Lichtenvoorde</li>
+        <li>hendrickhogendijkhovenier@gmail.com</li>
+    </ul>
+</div>
+<div class="div2">
+    <h3>openingstijden</h3>
+    <ul>
+        <li>Maandag - vrijdag: 07.00 - 17.00 uur</li>
+        <li>Zaterdag: Op afspraak</li>
+        <li>Zondag: Gesloten</li>
+    </ul>
+</div>
+<div class="div3"><img src="../assets/images/image.png" alt="locatie"></div>
+</footer>
