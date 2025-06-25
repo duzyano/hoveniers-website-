2254e05c309994fc9cf805478aa3b1b1f24658ff
MohammedK <m.ali.kassem@outlook.com>
2025-06-25 10:02:49 +0200

    added navbar.php

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet" href="assets/css/slideshow.css">
-  </head>
-  <body>
-    <section class="aboutme">
-      
-    </section>
-    <?php include 'includes/slideshow.php'; ?>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <link rel="stylesheet" href="assets/css/slideshow.css">
+</head>
+
+<body>
+  <section class="aboutme">
+
+  </section>
+  <?php include 'includes/slideshow.php'; ?>
+</body>
+
 </html>
