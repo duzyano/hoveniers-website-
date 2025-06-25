6c0d63208232e5f38fdf764328b1818ac39a7353
MohammedK <m.ali.kassem@outlook.com>
2025-06-25 19:00:02 +0200

    navbar finished

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,38 +1,61 @@
-<navbar class="navbar navbar--scroll">
-  <div class="navbar__container container">
-    <div class="navbar__logo">
-      <img src="./images/logo.svg" alt="Leno" />
-    </div>
-    <div class="navbar__mobile-menu">
+ <navbar class="navbar navbar--scroll">
+   <div class="navbar__container container">
+     <div class="navbar__logo">
+       <img src="./assets/images/logo3__1_-removebg-preview.png" alt="" width="300px" height="300px" />
+     </div>
+     <div class="navbar__menu">
+       <ul class="navbar__menu-list">
+         <li class="navbar__menu-item">
+           <a href="index.php" class="navbar__menu-link">Home</a>
+         </li>
+         <li class="navbar__menu-item">
+           <a href="index.php#aboutme" class="navbar__menu-link">About</a>
+         </li>
+         <li class="navbar__menu-item">
+           <a href="index.php#diensten" class="navbar__menu-link">Diensten</a>
+         </li>
+         <li class="navbar__menu-item">
+           <a href="index.php#projecten" class="navbar__menu-link">Projecten</a>
+         </li>
+         <li class="navbar__menu-item">
+           <a href="index.php#reviews" class="navbar__menu-link">Reviews</a>
+         </li>
+         <li class="navbar__menu-item">
+           <a href="index.php#contact" class="navbar__menu-link">Contact</a>
+         </li>
 
-      <div class="navbar__mobile-menu-toggle">
-        <i class="fas fa-bars fa-2x"></i>
-      </div>
-    </div>
-    <div class="navbar__menu">
+       </ul>
+     </div>
+     <!-- Mobile Menu & Hamburger Icon -->
+     <div class="navbar__mobile-menu">
+       <div class="navbar__mobile-menu-toggle">
+         <i class="fas fa-bars fa-2x"></i>
+       </div>
+       <!-- Mobile Menu Items -->
+       <div class="navbar__mobile-menu-items">
+         <ul class="navbar__mobile-menu-list">
+           <li class="navbar__mobile-menu-item">
+             <a href="index.php" class="navbar__mobile-menu-link">Home</a>
+           </li>
+           <li class="navbar__mobile-menu-item">
+             <a href="index.php#aboutme" class="navbar__mobile-menu-link">About</a>
+           </li>
+           <li class="navbar__mobile-menu-item">
+             <a href="index.php#diensten" class="navbar__mobile-menu-link">Diensten</a>
+           </li>
+           <li class="navbar__mobile-menu-item">
+             <a href="index.php#projecten" class="navbar__mobile-menu-link">Projecten</a>
+           </li>
+           <li class="navbar__menu-item">
+             <a href="index.php#reviews" class="navbar__mobile-menu-link">Reviews</a>
+           </li>
+           <li class="navbar__menu-item">
+             <a href="index.php#contact" class="navbar__mobile-menu-link">Contact</a>
+           </li>
 
-      <ul class="navbar__menu-list">
-        <li class="navbar__menu-item">
-          <a href="index.php" class="navbar__menu-link">Home</a>
-        </li>
-        <li class="navbar__menu-item">
-          <a href="#aboutme" class="navbar__menu-link">About</a>
-        </li>
-        <li class="navbar__menu-item">
-          <a href="#diensten" class="navbar__menu-link">Diensten</a>
-        </li>
-        <li class="navbar__menu-item">
-          <a href="#projecten" class="navbar__menu-link">Projecten</a>
-        </li>
-        <li class="navbar__menu-item">
-          <a href="#reviews" class="navbar__menu-link">Reviews</a>
-        </li>
-        <li class="navbar__menu-item">
-          <a href="#contact" class="navbar__menu-link">Contact</a>
-        </li>
-      </ul>
-    </div>
-    <!-- Mobile Menu & Hamburger Icon -->
-
-</navbar>
-<script src="./assets/js/navbar.js"></script>
+         </ul>
+       </div>
+     </div>
+   </div>
+ </navbar>
+ <script src="./assets/js/navbar.js"></script>
