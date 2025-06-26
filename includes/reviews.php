accedb36c2a661f5876cf45cd263d1f0d08297d0
valeriooooooo <valeriomalagoli08@gmail.com>
2025-06-26 15:17:23 +0200

    reviews klaar

--- includes/reviews.php
+++ includes/reviews.php
@@ -1,20 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <section class="reviews" id="reviews">
-<div class="reviews_container">
-    <div class="review_card"><p>“Heel erg bedankt voor de efficiënte service, je was heel snel klaar en ik zal je nummer zeker behouden om je weer te gebruiken.” Hella Hoes</p></div>
-    <div class="review_card"><p>“Ga alsjeblieft door met de bezoeken aan het huis van mijn vader, want je doet geweldig werk!” Henk Haak</p></div>
-    <div class="review_card"><p>“Zoals u weet ben ik altijd tevreden geweest met de service die u de afgelopen jaren heeft verleend. Vertel me alstublieft wanneer u klaar bent om volgend jaar weer te beginnen met het maaien van mijn gazons, aangezien ik graag uw diensten wil blijven ontvangen.” Hans Hogendijk</p></div>
-    <div class="review_card"><p>“Bedankt voor de grondige opruimbeurt die u aan mijn tuin heeft uitgevoerd. De tuin is er enorm van opgeknapt en weer bruikbaar gemaakt. Nogmaals bedankt.” Hugo van Heren</p></div>
-    <div class="review_card"><p>“Mijn tuin was een mijn jungle voor en achter. Ik ben zo blij met het resultaat; u heeft zo hard gewerkt en alles mooi en netjes achtergelaten. Aarzel niet om mijn opmerkingen te gebruiken in toekomstige advertenties. Nogmaals bedankt voor al je harde werk en efficiëntie!” Helga Hagel</p></div>
-    <div class="review_card"><p>“We zijn erg blij met ons nieuwe dak van de schuur en het andere werk dat tot een zeer hoge standaard is voltooid. Heel erg bedankt voor uw medewerking!” Hopke Havermout-Hoeksteen</p></div>
-</div>
-    </section>
-</body>
-</html>
+
+<section class="reviews" id="reviews">
+    <div class="container">
+        <div class="review_card">
+            <input type="checkbox" id="expanded">
+            <p>“Heel erg bedankt voor de efficiënte service, je was heel snel klaar en ik zal je nummer zeker behouden om je weer te gebruiken.” Hella Hoes</p>
+            <label for="expanded" role="button">read more</label>
+        </div>
+        <div class="review_card">
+            <input type="checkbox" id="expandedone">
+            <p>“Ga alsjeblieft door met de bezoeken aan het huis van mijn vader, want je doet geweldig werk!”
+
+Henk Haak“Ga alsjeblieft door met de bezoeken aan het huis van mijn vader, want je doet geweldig werk!”
+
+Henk Haak</p>
+            <label for="expandedone" role="button">read more</label>
+        </div>
+        <div class="review_card">
+            <input type="checkbox" id="expandedtwo">
+            <p>“Zoals u weet ben ik altijd tevreden geweest met de service die u de afgelopen jaren heeft verleend. Vertel me alstublieft wanneer u klaar bent om volgend jaar weer te beginnen met het maaien van mijn gazons, aangezien ik graag uw diensten wil blijven ontvangen.”
+
+Hans Hogendijk</p>
+            <label for="expandedtwo" role="button">read more</label>
+        </div>
+        <div class="review_card">
+            <input type="checkbox" id="expandedthree">
+            <p>“Bedankt voor de grondige opruimbeurt die u aan mijn tuin heeft uitgevoerd. De tuin is er enorm van opgeknapt en weer bruikbaar gemaakt. Nogmaals bedankt.”
+
+Hugo van Heren</p>
+            <label for="expandedthree" role="button">read more</label>
+        </div>
+        <div class="review_card">
+            <input type="checkbox" id="expandedfour">
+            <p>“Mijn tuin was een mijn jungle voor en achter. Ik ben zo blij met het resultaat; u heeft zo hard gewerkt en alles mooi en netjes achtergelaten. Aarzel niet om mijn opmerkingen te gebruiken in toekomstige advertenties. Nogmaals bedankt voor al je harde werk en efficiëntie!”
+
+Helga Hagel</p>
+            <label for="expandedfour" role="button">read more</label>
+        </div>
+        <div class="review_card">
+            <input type="checkbox" id="expandedfive">
+            <p>“We zijn erg blij met ons nieuwe dak van de schuur en het andere werk dat tot een zeer hoge standaard is voltooid. Heel erg bedankt voor uw medewerking!”
+
+Hopke Havermout-Hoeksteen</p>
+            <label for="expandedfive" role="button">read more</label>
+        </div>
+    </div>
+</section>
