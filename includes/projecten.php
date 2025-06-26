5e032649c97fc328d1dea64151dbc48f0b5aaca6
MohammedK <m.ali.kassem@outlook.com>
2025-06-26 16:11:29 +0200

    file projects added

--- includes/projecten.php
+++ includes/projecten.php
@@ -1,5 +1,5 @@
 <section class="projects container">
-  <div class="projects__image"><img src="" alt=""></div>
+  <div class="projects__image"><img src="./assets/images/a8db60_0f63450d75f645bdab574047c56cf898_mv2.webp" alt=""></div>
   <div class="projects__image"><img src="" alt=""></div>
   <div class="projects__image"><img src="" alt=""></div>
   <div class="projects__image"><img src="" alt=""></div>
