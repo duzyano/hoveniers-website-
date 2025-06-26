431afa40d53bcf8c979b2575f917a962ded2fdb9
MohammedK <m.ali.kassem@outlook.com>
2025-06-26 22:28:22 +0200

    added images gallery

--- includes/projecten.php
+++ includes/projecten.php
@@ -1,11 +1,10 @@
-<section class="projects container">
-  <div class="projects__image"><img src="./assets/images/a8db60_0f63450d75f645bdab574047c56cf898_mv2.webp" alt=""></div>
-  <div class="projects__image"><img src="" alt=""></div>
-  <div class="projects__image"><img src="" alt=""></div>
-  <div class="projects__image"><img src="" alt=""></div>
-  <div class="projects__image"><img src="" alt=""></div>
-  <div class="projects__image"><img src="" alt=""></div>
-  <div class="projects__image"><img src="" alt=""></div>
-  <div class="projects__image"><img src="" alt=""></div>
-  <div class="projects__image"><img src="" alt=""></div>
+<section class="projects">
+  <div class="container">
+    <img src="./assets/images/a8db60_0f63450d75f645bdab574047c56cf898_mv2.webp" alt="">
+    <img class="image-2" src="./assets/images/a8db60_9f168361c12f4ad5800e1acf22f49080_mv2 (1).jpeg" alt="">
+    <img src="./assets/images/a8db60_41e28b52fdc8461dbe9e00368c7cf8d4_mv2.jpeg" alt="">
+    <img src="./assets/images/a8db60_87f9493ee50244389acb45a8ed75afe4_mv2.webp" alt="">
+    <img class="image-5" src="./assets/images/a8db60_379dcd1d20d04347b8fac28edbeb45c4_mv2.jpeg" alt="">
+    <img src="./assets/images/a8db60_89729f86eabe4bf3b65d2f554cfecd6d_mv2.webp" alt="">
+  </div>
 </section>
