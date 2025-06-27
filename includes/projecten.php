7e10e1a47fd65c192ee51c071a9e8e1161cd6660
MohammedK <m.ali.kassem@outlook.com>
2025-06-27 22:28:58 +0200

    projecten.php somewhat responsive

--- includes/projecten.php
+++ includes/projecten.php
@@ -1,10 +1,10 @@
 <section class="projects">
   <div class="container">
-    <img src="./assets/images/a8db60_0f63450d75f645bdab574047c56cf898_mv2.webp" alt="">
-    <img class="image-2" src="./assets/images/a8db60_9f168361c12f4ad5800e1acf22f49080_mv2 (1).jpeg" alt="">
-    <img src="./assets/images/a8db60_41e28b52fdc8461dbe9e00368c7cf8d4_mv2.jpeg" alt="">
-    <img src="./assets/images/a8db60_87f9493ee50244389acb45a8ed75afe4_mv2.webp" alt="">
+    <img class="image-1" src="./assets/images/a8db60_0f63450d75f645bdab574047c56cf898_mv2.webp" alt="">
+    <img class="image-2" src="./assets/images/a8db60_e7ba32f754a8415588e6327b5647b8d6_mv2.webp" alt="">
+    <img class="image-3" src="./assets/images/a8db60_41e28b52fdc8461dbe9e00368c7cf8d4_mv2.jpeg" alt="">
+    <img class="image-4" src="./assets/images/a8db60_87f9493ee50244389acb45a8ed75afe4_mv2.webp" alt="">
     <img class="image-5" src="./assets/images/a8db60_379dcd1d20d04347b8fac28edbeb45c4_mv2.jpeg" alt="">
-    <img src="./assets/images/a8db60_89729f86eabe4bf3b65d2f554cfecd6d_mv2.webp" alt="">
+    <img class="image-6" src="./assets/images/a8db60_89729f86eabe4bf3b65d2f554cfecd6d_mv2.webp" alt="">
   </div>
 </section>
