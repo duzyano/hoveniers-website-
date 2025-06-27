cebee25a7133fe714bc95a6fe6f026f1fab922f4
valeriooooooo <valeriomalagoli08@gmail.com>
2025-06-27 13:07:52 +0200

    reviews aanpasingen

--- includes/reviews.php
+++ includes/reviews.php
@@ -1,4 +1,3 @@
-
 <section class="reviews" id="reviews">
     <div class="container">
         <div class="review_card">
@@ -10,37 +9,37 @@
             <input type="checkbox" id="expandedone">
             <p>“Ga alsjeblieft door met de bezoeken aan het huis van mijn vader, want je doet geweldig werk!”
 
-Henk Haak“Ga alsjeblieft door met de bezoeken aan het huis van mijn vader, want je doet geweldig werk!”
+                Henk Haak“Ga alsjeblieft door met de bezoeken aan het huis van mijn vader, want je doet geweldig werk!”
 
-Henk Haak</p>
+                Henk Haak</p>
             <label for="expandedone" role="button">read more</label>
         </div>
         <div class="review_card">
             <input type="checkbox" id="expandedtwo">
             <p>“Zoals u weet ben ik altijd tevreden geweest met de service die u de afgelopen jaren heeft verleend. Vertel me alstublieft wanneer u klaar bent om volgend jaar weer te beginnen met het maaien van mijn gazons, aangezien ik graag uw diensten wil blijven ontvangen.”
 
-Hans Hogendijk</p>
+                Hans Hogendijk</p>
             <label for="expandedtwo" role="button">read more</label>
         </div>
         <div class="review_card">
             <input type="checkbox" id="expandedthree">
             <p>“Bedankt voor de grondige opruimbeurt die u aan mijn tuin heeft uitgevoerd. De tuin is er enorm van opgeknapt en weer bruikbaar gemaakt. Nogmaals bedankt.”
 
-Hugo van Heren</p>
+                Hugo van Heren</p>
             <label for="expandedthree" role="button">read more</label>
         </div>
         <div class="review_card">
             <input type="checkbox" id="expandedfour">
             <p>“Mijn tuin was een mijn jungle voor en achter. Ik ben zo blij met het resultaat; u heeft zo hard gewerkt en alles mooi en netjes achtergelaten. Aarzel niet om mijn opmerkingen te gebruiken in toekomstige advertenties. Nogmaals bedankt voor al je harde werk en efficiëntie!”
 
-Helga Hagel</p>
+                Helga Hagel</p>
             <label for="expandedfour" role="button">read more</label>
         </div>
         <div class="review_card">
             <input type="checkbox" id="expandedfive">
             <p>“We zijn erg blij met ons nieuwe dak van de schuur en het andere werk dat tot een zeer hoge standaard is voltooid. Heel erg bedankt voor uw medewerking!”
 
-Hopke Havermout-Hoeksteen</p>
+                Hopke Havermout-Hoeksteen</p>
             <label for="expandedfive" role="button">read more</label>
         </div>
     </div>
