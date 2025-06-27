e3b54e2440d8ae2e657afae3e552a3e50ae1821c
duzyano <Duzyanodrj@hotmail.com>
2025-06-27 13:00:08 +0100

    slidshow af met aaanspasingen van hendrick

--- includes/slideshow.php
+++ includes/slideshow.php
@@ -5,25 +5,33 @@ var slideIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
   var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    // x[i].style.display = "none";
-     x[i].style.transform = "scale(0.8)";
-  }
+
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1;}
-  if (x.length > 0) {
-    // x[slideIndex-1].style.display = "block";
-    x[slideIndex-1].style.transform = "scale(1)";
+  if (slideIndex >= x.length) 
+  {
+    slideIndex = 0;
   }
-  setTimeout(carousel, 5000); // Change image every 5 seconds
+
+  if (x.length > 0) {
+    const currentActive = document.querySelector(".mySlides.active");
+    currentActive.setAttribute("z-index", "1");
+
+
+    x[slideIndex].setAttribute("z-index", "2");
+    x[slideIndex].classList.add("active");
+
+    setTimeout(() => {
+      currentActive.classList.remove("active");
+    }, 1000);
+  }
+  
+  setTimeout(carousel, 5000);
 }
 </script>
 <div class="slider">
-<img class="mySlides" src="../assets/images/a8db60_9f168361c12f4ad5800e1acf22f49080_mv2 (1).jpeg">
-<img class="mySlides" src="../assets/images/a8db60_379dcd1d20d04347b8fac28edbeb45c4_mv2.jpeg">
-<img class="mySlides" src="../assets/images/a8db60_e7ba32f754a8415588e6327b5647b8d6_mv2.webp">
+<img class="mySlides active" src="./assets/images/a8db60_9f168361c12f4ad5800e1acf22f49080_mv2 (1).jpeg">
+<img class="mySlides" src="./assets/images/a8db60_379dcd1d20d04347b8fac28edbeb45c4_mv2.jpeg">
+<img class="mySlides" src="./assets/images/a8db60_e7ba32f754a8415588e6327b5647b8d6_mv2.webp">
 <h1>Welkom bij de website van Hendrik Hogendijk, hovenier in de regio Utrecht, Zeist en de Bilt.</h1>
 </div>
-
