329270910a15d3b5a55f24a0a8713f9013c9b5a3
MohammedK <m.ali.kassem@outlook.com>
2025-06-30 11:10:46 +0200

    added contact.php, contact.css

---
+++ includes/contact.php
@@ -0,0 +1,5 @@
+<section class="contact-form">
+  <p class="contact-form__intro">
+    Ik streef ernaar om constant in contact te staan met onze klanten totdat de klus geklaard is. Als u vragen of speciale verzoeken heeft, stuur ons dan een bericht. Voor een vrijblijvende offerte kunt u contact met ons opnemen wanneer het u uitkomt. Wij zijn u graag van dienst!
+  </p>
+</section>
