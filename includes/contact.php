5b6bdc44859b9cdc7892bdf3944a9a98908ffc4d
MohammedK <m.ali.kassem@outlook.com>
2025-07-01 15:03:27 +0200

    contact

--- includes/contact.php
+++ includes/contact.php
@@ -1,5 +1,32 @@
 <section class="contact-form">
+
+  <h2 class="contact__title">Neem vrijblijvend contact</h2>
   <p class="contact-form__intro">
     Ik streef ernaar om constant in contact te staan met onze klanten totdat de klus geklaard is. Als u vragen of speciale verzoeken heeft, stuur ons dan een bericht. Voor een vrijblijvende offerte kunt u contact met ons opnemen wanneer het u uitkomt. Wij zijn u graag van dienst!
   </p>
+
+  <form
+    action="https://formspree.io/f/xzzgoooy"
+    method="POST">
+    <div class="formgroup">
+      <label for="first-name" class="visually-hidden">Naam</label>
+      <input type="text" name="first-name" id="first-name" placeholder="Naam">
+    </div>
+    <div class="formgroup">
+      <label for="email" class="visually-hidden">Email</label>
+      <input type="email" name="email" id="email" placeholder="Email">
+    </div>
+    <div class="formgroup">
+      <label for="number" class="visually-hidden">tel.nr</label>
+      <input type="tel" name="number" id="number" placeholder="Telefoonnummer">
+    </div>
+    <div class="formgroup">
+      <label for="message" class="visually-hidden">Opmerking</label>
+      <textarea name="message" id="message" rows="10" placeholder="Hebt u een opmerking?"></textarea>
+    </div>
+    <div class="formgroup">
+      <button type="submit" class="btn">Verzend</button>
+    </div>
+  </form>
 </section>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum pariatur minus obcaecati! Eveniet modi error in natus ut temporibus porro doloribus minus. Eius, unde saepe laboriosam fuga necessitatibus delectus dolores, consequatur quidem tempore maiores eum obcaecati repellendus incidunt architecto debitis aspernatur numquam accusamus, reiciendis dignissimos sit sint pariatur esse ut. Recusandae aspernatur culpa odio quibusdam, ipsum voluptatem natus, quo hic repellendus sapiente sed? Voluptatem omnis tenetur sapiente, dicta labore harum eum quae aliquam ipsam cumque dolores, beatae nulla doloribus atque. Labore, voluptatum. Ad ipsum quod sed harum, in, velit nisi deleniti reiciendis, mollitia voluptates ut dolor? Voluptatibus ratione vero libero.</p>
