2485a434920f41c865e0b35a664e7e96391da4a2
MohammedK <m.ali.kassem@outlook.com>
2025-07-03 01:56:14 +0200

    made diensten section responsive

--- includes/diensten.php
+++ includes/diensten.php
@@ -1,21 +1,25 @@
 <section class="diensten" id="diensten">
   <h2 class="diensten__title">Mijn Diensten</h2>
   <div class="diensten__cards">
-    <div class="diensten__card"><img src="./assets/images/icon-tuinaanleg.png">
-      <p class="titel">Tuinaanleg </p>
-      <p>Het aanleggen van een tuin doen ik altijd vanuit dezelfde drive: ik wil er iets moois van maken.</p>
+    <div class="diensten__card">
+      <img src="./assets/images/icon-tuinaanleg.png">
+      <h4 class="diensten__card-title">Tuinaanleg </h4>
+      <p class="diensten__card-description">Het aanleggen van een tuin doen ik altijd vanuit dezelfde drive: ik wil er iets moois van maken.</p>
     </div>
-    <div class="diensten__card"><img src="./assets/images/icon-tuinonderhoud.png">
-      <p class="titel">Tuinonderhoud</p>
-      <p>Een mooie tuin verdient het juiste onderhoud. Immers, goed onderhoud is een essentiële voorwaarde voor een tuin die het hele jaar door mooi blijft. Het onderhouden van een tuin is net zo belangrijk als het aanleggen ervan. Ik zorg ervoor dat uw tuin in topconditie blijft.</p>
+    <div class="diensten__card">
+      <img src="./assets/images/icon-tuinonderhoud.png">
+      <h4 class="diensten__card-title">Tuinonderhoud</h4>
+      <p class="diensten__card-description">Een mooie tuin verdient het juiste onderhoud. Immers, goed onderhoud is een essentiële voorwaarde voor een tuin die het hele jaar door mooi blijft. Het onderhouden van een tuin is net zo belangrijk als het aanleggen ervan. Ik zorg ervoor dat uw tuin in topconditie blijft.</p>
     </div>
-    <div class="diensten__card"><img src="./assets/images/icon-tuinontwerp.png">
-      <p class="titel">Tuinontwerp</p>
-      <p>Een mooie tuin valt of staat bij een goed tuinontwerp. Als het op ontwerpen aankomt, maakt Hendrik Hogendijk het verschil.</p>
+    <div class="diensten__card">
+      <img src="./assets/images/icon-tuinontwerp.png">
+      <h4 class="diensten__card-title">Tuinontwerp</h4>
+      <p class="diensten__card-description">Een mooie tuin valt of staat bij een goed tuinontwerp. Als het op ontwerpen aankomt, maakt Hendrik Hogendijk het verschil.</p>
     </div>
-    <div class="diensten__card"><img src="./assets/images/icon-zwemvijvers.png">
-      <p class="titel">Zwemvijvers</p>
-      <p>Zwemvijvers, is één van mijn grootste passies. En gelukkig, ze winnen aan steeds meer populariteit, waardoor ik er veel ervaring mee heb opgebouwd.</p>
+    <div class="diensten__card">
+      <img src="./assets/images/icon-zwemvijvers.png">
+      <h4 class="diensten__card-title">Zwemvijvers</h4>
+      <p class="diensten__card-description">Zwemvijvers, is één van mijn grootste passies. En gelukkig, ze winnen aan steeds meer populariteit, waardoor ik er veel ervaring mee heb opgebouwd.</p>
     </div>
   </div>
 </section>
