993904d463ef1e9caa73a7b692f838fbf0594c2d
MohammedK <m.ali.kassem@outlook.com>
2025-07-02 10:00:27 +0200

    done

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
+  <link rel="icon" href="./assets/images/apple-touch-icon.png">
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
