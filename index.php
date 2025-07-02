5d216d6815995cce95b4bbc87de4517c616f24be
MohammedK <m.ali.kassem@outlook.com>
2025-07-02 20:55:01 +0200

    fixed navbar

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
+
   <link rel="icon" href="./assets/images/apple-touch-icon.png">
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
@@ -15,6 +16,7 @@
   <?php include 'includes/slideshow.php'; ?>
   <?php include 'includes/about-me.php'; ?>
   <?php include 'includes/diensten.php'; ?>
+  <?php include 'includes/projecten.php'; ?>
 
 
 </body>
