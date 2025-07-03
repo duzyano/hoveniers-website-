d69c93cb35baf4b722deaab47a639d8dda67fb6f
MohammedK <m.ali.kassem@outlook.com>
2025-07-03 13:00:41 +0200

    footer responsive

--- includes/footer.php
+++ includes/footer.php
@@ -23,6 +23,8 @@
             <img src="./assets/images/logo3__2_-removebg-preview.png" alt="logo">
         </div>
     </div>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2447.91639719393!2d4.487097190439704!3d52.15403009968802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5c68c86a8ade7%3A0x71dd403ebb677e23!2sHoefstraat%2042%2C%202311%20PT%20Leiden!5e0!3m2!1sen!2snl!4v1750927492926!5m2!1sen!2snl" width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <p class="footer__copyright">© 2023 Hendrik Hogendijk. Alle rechten voorbehouden.</p>
+    <div class="footer__iframe">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2447.91639719393!2d4.487097190439704!3d52.15403009968802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5c68c86a8ade7%3A0x71dd403ebb677e23!2sHoefstraat%2042%2C%202311%20PT%20Leiden!5e0!3m2!1sen!2snl!4v1750927492926!5m2!1sen!2snl" width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <small class="footer__copyright">© 2023 Hendrik Hogendijk. Alle rechten voorbehouden.</small>
 </footer>
