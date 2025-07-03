2d0328b9057589b4ba5f53ee424cc507a5205741
MohammedK <m.ali.kassem@outlook.com>
2025-07-03 05:22:48 +0200

    footer responsive

--- includes/footer.php
+++ includes/footer.php
@@ -1,25 +1,29 @@
-<footer>
+<footer class="footer">
 
-    <div class="div1">
-        <h3>Gegevens</h3>
-        <ul>
-            <li><a href="tel:0621745678">06-21745678</a></li>
-            <li><a href="mailto:hendrickhogendijkhovenier@gmail.com">hendrickhogendijkhovenier@gmail.com</a></li>
-            <li>Hoefstraat 42</li>
-            <li>7143HH Lichtenvoorde</li>
+    <div class="footer__container">
+        <div class="footer__gegevens">
+            <h3 class="footer__gegevens-title">Gegevens</h3>
+            <ul class="footer__list">
+                <li class="footer__list-items"><a href="tel:0621745678" class="footer__list-link">06-21745678</a></li>
+                <li class="footer__list-items"><a href="mailto:hendrickhogendijkhovenier@gmail.com" class="footer__list-link">hendrickhogendijkhovenier@gmail.com</a></li>
+                <li class="footer__list-items">Hoefstraat 42</li>
+                <li class="footer__list-items">7143HH Lichtenvoorde</li>
 
-        </ul>
+            </ul>
+        </div>
+        <div class="footer__tijden">
+            <h3 class="footer__tijden-title">openingstijden</h3>
+            <ul class="footer__list">
+                <li class="footer__list-items">Maandag - vrijdag: 07.00 - 17.00 uur</li>
+                <li class="footer__list-items">Zaterdag: Op afspraak</li>
+                <li class="footer__list-items">Zondag: Gesloten</li>
+            </ul>
+        </div>
+        <div class="footer__logo">
+            <img src="./assets/images/logo3__2_-removebg-preview.png" alt="logo">
+        </div>
     </div>
-    <div class="div2">
-        <h3>openingstijden</h3>
-        <ul>
-            <li>Maandag - vrijdag: 07.00 - 17.00 uur</li>
-            <li>Zaterdag: Op afspraak</li>
-            <li>Zondag: Gesloten</li>
-        </ul>
-    </div>
-    <!-- <div class="locatie"><img src="../assets/images/image.png" alt="locatie"></div> -->
-    <div class="logo"><img src="./assets/images/logo3__2_-removebg-preview.png" alt="logo"></div>
+
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2447.91639719393!2d4.487097190439704!3d52.15403009968802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5c68c86a8ade7%3A0x71dd403ebb677e23!2sHoefstraat%2042%2C%202311%20PT%20Leiden!5e0!3m2!1sen!2snl!4v1750927492926!5m2!1sen!2snl" width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 </footer>
